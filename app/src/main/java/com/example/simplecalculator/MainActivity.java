4b783e917c29b89b58c5d3fee8e240cf8977e0f9
Adheesh <adheeshpandey49@gmail.com>
2025-04-15 13:56:23 +1000

    Code written for execution

--- app/src/main/java/com/example/simplecalculator/MainActivity.java
+++ app/src/main/java/com/example/simplecalculator/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.simplecalculator;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +23,53 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
     }
-}
+    public void Add(View v){
+        EditText et1 = (EditText)findViewById(R.id.editTextNumber);
+        EditText et2 = (EditText)findViewById(R.id.editTextNumber1);
+        EditText et3 = (EditText)findViewById(R.id.editTextNumber2);
+
+        int n1 = Integer.parseInt(et1.getText().toString());
+        int n2 = Integer.parseInt(et1.getText().toString());
+        int result = n1 + n2;
+
+        et3.setText("Total Value " + result);
+
+    }
+    public void Subtract(View v) {
+        EditText et1 = (EditText) findViewById(R.id.editTextNumber);
+        EditText et2 = (EditText) findViewById(R.id.editTextNumber1);
+        EditText et3 = (EditText) findViewById(R.id.editTextNumber2);
+
+        int n1 = Integer.parseInt(et1.getText().toString());
+        int n2 = Integer.parseInt(et1.getText().toString());
+        int result = n1 - n2;
+
+        et3.setText("Subtract Value " + result);
+    }
+    public void Multiply(View v) {
+        EditText et1 = (EditText) findViewById(R.id.editTextNumber);
+        EditText et2 = (EditText) findViewById(R.id.editTextNumber1);
+        EditText et3 = (EditText) findViewById(R.id.editTextNumber2);
+
+        int n1 = Integer.parseInt(et1.getText().toString());
+        int n2 = Integer.parseInt(et1.getText().toString());
+        int result = n1 * n2;
+
+        et3.setText("Multiply Value " + result);
+    }
+    public void DDivide(View v) {
+        EditText et1 = (EditText) findViewById(R.id.editTextNumber);
+        EditText et2 = (EditText) findViewById(R.id.editTextNumber1);
+        EditText et3 = (EditText) findViewById(R.id.editTextNumber2);
+
+        int n1 = Integer.parseInt(et1.getText().toString());
+        int n2 = Integer.parseInt(et1.getText().toString());
+        int result = n1 / n2;
+
+        et3.setText("Division Value " + result);
+    }
+
+    }
